115ed92db79d2eef2539cb397e28311020f4079d
tp <piskorski24@gmail.com>
2022-07-13 18:11:31 +0200

    2day

---
+++ 2day.py
@@ -0,0 +1,5 @@
+print("Welcome to the tip calculator!")
+bill=input("What was the total bill?")
+people=input("How many people split the bill?")
+percent = input("What was the tip percentage 10, 12, 15?")
+print("Each person should pay:", (float(bill) * float(percent) / float(people))/100)
